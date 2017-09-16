5dde2a82988894e0d733f4ec443c3563cfe65711
Brad Conley <brad.conl3y@gmail.com>
2017-09-16 09:50:03 -0400

    lazy commit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
+import map from 'lodash/map'
 import './index.css';
 
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      city: "",
+      forecast: [],
+    }
+  }
 
   componentDidMount(){
-    console.log(process.env.REACT_APP_WEATHER_API)
     fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=33705,us&appid=${process.env.REACT_APP_WEATHER_API}`)
-    .then((res) => {
+    .then(res => {
       return res.json()
     })
-    .then((data) => {
+    .then(data => {
       console.log(data)
+      this.setState({ forecast: data.list, city: data.city.name })
     })
   }
 
   render() {
     return (
       <div>
-        <h1>Weatherzzz</h1>
+        <h1>Weatherrrzzz</h1>
+        {map(this.state.forecast, (weather) => (
+            <div key={weather.dt}>
+              <p>{weather}</p>
+            </div>
+        ))}
       </div>
     );
   }
